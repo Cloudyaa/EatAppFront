d6d77ececb5db9bbfb854f3ef80f7fbbd265730b
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-08 13:43:10 +0000

    Refactored return and added text next to icon (on mobile)

--- src/components/Navbar/NavbarParts/NavActions.tsx
+++ src/components/Navbar/NavbarParts/NavActions.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
-import { IconButton } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import { NavbarPartWrapper } from './NavbarPartWrapper';
 import { LocalGroceryStoreOutlined, PersonOutlineOutlined } from '@mui/icons-material';
 import { Searchbar } from './Searchbar';
 import { NavLink } from 'react-router-dom';
+import { useViewport } from '../../../hooks';
 
 export const NavActions = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const { viewportWidth, breakpoint } = useViewport();
+
+  useEffect(() => {
+    viewportWidth < breakpoint ? setIsMobile(true) : setIsMobile(false);
+  }, [viewportWidth]);
+
+  const navActions = [
+    {
+      name: <p>My account</p>,
+      to: '/user/login',
+      icon: <PersonOutlineOutlined />,
+    },
+
+    {
+      name: <p>Basket</p>,
+      to: '/user/checkout',
+      icon: <LocalGroceryStoreOutlined />,
+    },
+  ];
+
   return (
     <NavbarPartWrapper classes="navbar__actions">
       <Searchbar />
-      <IconButton>
-        <NavLink to="/user/login">
-          <PersonOutlineOutlined />
-        </NavLink>
-      </IconButton>
-      <IconButton>
-        <LocalGroceryStoreOutlined />
-      </IconButton>
+
+      {navActions.map(({ name, to, icon }, index) => {
+        return (
+          <NavLink to={to} key={index} className="navbar__actions-element">
+            {icon}
+            {isMobile ? name : null}
+          </NavLink>
+        );
+      })}
     </NavbarPartWrapper>
   );
 };
