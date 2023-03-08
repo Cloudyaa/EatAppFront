4d11b23fb72172d33381eeddc0ebeb0fcb777fbf
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-08 13:54:47 +0000

    Moved breakpoint const to hook

--- src/hooks/useViewport.ts
+++ src/hooks/useViewport.ts
@@ -2,12 +2,12 @@ import { useEffect, useState } from 'react';
 
 export const useViewport = () => {
   const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
-
+  const breakpoint = 800;
   useEffect(() => {
     const handleWindowResize = () => setViewportWidth(window.innerWidth);
     window.addEventListener('resize', handleWindowResize);
     return () => window.removeEventListener('resize', handleWindowResize);
   }, []);
 
-  return { viewportWidth };
+  return { viewportWidth, breakpoint };
 };
