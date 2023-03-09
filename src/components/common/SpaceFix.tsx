8fbf7cc9b297acb966fd7ff679f576a22da92015
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-09 13:25:52 +0000

    Changed SpaceFix size to be more responsive

--- src/components/common/SpaceFix.tsx
+++ src/components/common/SpaceFix.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import { Box } from '@mui/material';
 
 // add space after navbar
-export const SpaceFix = () => <Box sx={{ height: '56px' }} />;
+export const SpaceFix = () => <Box sx={{ height: '8vh', position: 'relative' }} />;
