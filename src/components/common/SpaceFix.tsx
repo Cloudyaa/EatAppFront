807c0d16dbe3b39836f6d70b18144391af9ba8d2
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-09 20:12:59 +0000

    Changed SpaceFix size to be different depending on viewport

--- src/components/common/SpaceFix.tsx
+++ src/components/common/SpaceFix.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import { useViewport } from '../../hooks';
 
 // add space after navbar
-export const SpaceFix = () => <Box sx={{ height: '8vh', position: 'relative' }} />;
+export const SpaceFix = () => {
+  const { viewportWidth, breakpoint } = useViewport();
+
+  const height = viewportWidth > breakpoint ? '4vh' : '8vh';
+
+  return <Box sx={{ height: { height }, position: 'relative' }} />;
+};
