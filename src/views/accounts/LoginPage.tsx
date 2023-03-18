833e0403155ea9e21c3895db489a3010dbbb945e
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-18 12:43:32 +0000

    Added LoginPage structure

--- src/views/accounts/LoginPage.tsx
+++ src/views/accounts/LoginPage.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { SectionHeader, SectionWrapper } from 'components';
+import { SectionId } from 'frontTypes';
+import { LoginForm } from '../../components/Forms';
+import { Box } from '@mui/material';
 
 export const LoginPage = () => {
   return (
-    <div className="login_view">
-      <h1>Login page</h1>
-    </div>
+    <SectionWrapper id={SectionId.about} classes="accounts__login">
+      <SectionHeader>Welcome back!</SectionHeader>
+      <Box sx={{ width: '35ch', margin: '0 auto' }}>
+        <LoginForm />
+      </Box>
+    </SectionWrapper>
   );
 };
