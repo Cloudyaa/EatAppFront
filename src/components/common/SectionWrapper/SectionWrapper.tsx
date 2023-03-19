09577eb20b240f0ff360dd1173bfcf81c3393b01
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-19 00:20:19 +0000

    Added SpaceFix to section

--- src/components/common/SectionWrapper/SectionWrapper.tsx
+++ src/components/common/SectionWrapper/SectionWrapper.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { SectionId } from 'frontTypes';
+import { SpaceFix } from '../SpaceFix';
 
 interface Props {
   classes: string;
-  id: SectionId;
+  id?: SectionId;
   children: React.ReactNode;
 }
 
 export const SectionWrapper = ({ classes, id, children }: Props) => (
   <section id={id} className={classes}>
+    <SpaceFix />
     <Box className="container">{children}</Box>
   </section>
 );
-
