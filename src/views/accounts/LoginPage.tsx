9f5fe520d9a6a87acd8635d7d4833071ae42f066
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-19 00:27:51 +0000

    Removed mistaken id from wrapper

--- src/views/accounts/LoginPage.tsx
+++ src/views/accounts/LoginPage.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { SectionHeader, SectionWrapper, LoginForm } from 'components';
-import { SectionId } from 'frontTypes';
 
 export const LoginPage = () => {
   return (
-    <SectionWrapper id={SectionId.about} classes="accounts__login">
+    <SectionWrapper classes="accounts__login">
       <SectionHeader>Welcome back!</SectionHeader>
       <LoginForm />
     </SectionWrapper>
