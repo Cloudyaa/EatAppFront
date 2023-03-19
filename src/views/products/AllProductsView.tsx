ee4205f613873dff761beddbe595ca76dec431e3
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-19 22:08:24 +0000

    Finished whole structure for all products view

--- src/views/products/AllProductsView.tsx
+++ src/views/products/AllProductsView.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  ProductCardMedium,
+  SectionHeader,
+  SectionWrapper,
+  SkeletonMedium,
+  SpaceFix,
+} from 'components';
+import { apiUrl } from '../../config';
+import { SimpleProductEntity } from 'types';
 
 export const AllProductsView = () => {
+  const [products, setProducts] = useState<SimpleProductEntity[] | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(`${apiUrl}/products/search`);
+        const data: SimpleProductEntity[] = await res.json();
+        setProducts(data);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
+  const skeletons: JSX.Element[] = [];
+  for (let i = 0; i < 3; i++) {
+    skeletons.push(<SkeletonMedium />);
+  }
+
   return (
-    <div className="all_products_view">
-      <h1>Logged user page</h1>
-    </div>
+    <SectionWrapper classes="products__all">
+      <SectionHeader>All products</SectionHeader>
+      <div className="auto-grid-container">
+        {products === null ? (
+          skeletons
+        ) : (
+          <>
+            {products.map((one) => (
+              <ProductCardMedium name={one.name} price={one.price} key={one.productId} />
+            ))}
+          </>
+        )}
+      </div>
+      <SpaceFix />
+    </SectionWrapper>
   );
 };
