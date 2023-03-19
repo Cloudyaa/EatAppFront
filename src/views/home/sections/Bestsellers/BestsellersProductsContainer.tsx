3dc676e913d51397b2bf237865de35604f2b7c49
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-19 20:46:43 +0000

    Added fetch to load bestsellers from DB

--- src/views/home/sections/Bestsellers/BestsellersProductsContainer.tsx
+++ src/views/home/sections/Bestsellers/BestsellersProductsContainer.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
-import { ProductCardMedium } from 'components';
+import React, { useEffect, useState } from 'react';
+import { ProductCardMedium, SkeletonMedium } from 'components';
+import { apiUrl } from '../../../../config';
+import { SimpleProductEntity } from 'types';
 
 export const BestsellersProductsContainer = () => {
+  const [bestsellers, setBestsellers] = useState<SimpleProductEntity[] | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(`${apiUrl}/products/bestsellers`);
+        const data: SimpleProductEntity[] = await res.json();
+        setBestsellers(data);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
+  const skeletons: JSX.Element[] = [];
+  for (let i = 0; i < 3; i++) {
+    skeletons.push(<SkeletonMedium />);
+  }
+
   return (
     <div className="auto-grid-container bestsellers__cards-container">
-      <ProductCardMedium name="tomatoes" price={3.49} />
-      <ProductCardMedium name="carrots" price={3.49} />
-      <ProductCardMedium name="lettuce" price={3.49} />
+      {bestsellers === null ? (
+        skeletons
+      ) : (
+        <>
+          {bestsellers.map((one) => (
+            <ProductCardMedium name={one.name} price={one.price} key={one.productId} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
