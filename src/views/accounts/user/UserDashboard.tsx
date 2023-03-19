0651808902cb3cf7038e8576310e3b7374eb6fc7
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-19 14:40:07 +0000

    Build user dashboard starter

--- src/views/accounts/user/UserDashboard.tsx
+++ src/views/accounts/user/UserDashboard.tsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { SafeUserEntity, ErrorResponse } from 'types';
+import { apiUrl } from '../../../config';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import { ButtonFull, SectionHeader, SectionWrapper, SubPageWrapper } from 'components';
+import { Box } from '@mui/material';
 
 export const UserDashboard = () => {
+  const [user, setUser] = useState<SafeUserEntity | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const { user_id } = useParams();
+  const navigate = useNavigate();
+
+  const [cookies, setCookie, removeCookie] = useCookies();
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`${apiUrl}/user/dashboard/${user_id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${cookies.token}`,
+        },
+      });
+
+      if (res.ok) {
+        const data: SafeUserEntity = await res.json();
+        console.log(data);
+        setUser(data);
+      } else {
+        const data: ErrorResponse = await res.json();
+        if ('message' in data) {
+          setErrorMessage(data.message);
+        }
+      }
+    })();
+  }, []);
+
+  const handleLogout = () => {
+    removeCookie('token', { path: '/' });
+    navigate('/');
+  };
+
   return (
-    <div className="logged_user_view">
-      <h1>User dashboard</h1>
-    </div>
+    <SectionWrapper classes="user__dashboard">
+      {!user ? (
+        errorMessage
+      ) : (
+        <>
+          <SectionHeader>My account</SectionHeader>
+          <SubPageWrapper>
+            <p>Created at: {user.createdAt}</p>
+            <Box sx={{ maxWidth: '40ch' }}>
+              <ButtonFull onClick={handleLogout}>Log out</ButtonFull>
+            </Box>
+          </SubPageWrapper>
+        </>
+      )}
+    </SectionWrapper>
   );
 };
