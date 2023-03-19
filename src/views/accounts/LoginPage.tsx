d38264d94da19d0c4c3046f70e326c1e861efdce
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-19 00:22:28 +0000

    Improved imports

--- src/views/accounts/LoginPage.tsx
+++ src/views/accounts/LoginPage.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { SectionHeader, SectionWrapper } from 'components';
+import { SectionHeader, SectionWrapper, LoginForm } from 'components';
 import { SectionId } from 'frontTypes';
-import { LoginForm } from '../../components/Forms';
 
 export const LoginPage = () => {
-
   return (
     <SectionWrapper id={SectionId.about} classes="accounts__login">
       <SectionHeader>Welcome back!</SectionHeader>
