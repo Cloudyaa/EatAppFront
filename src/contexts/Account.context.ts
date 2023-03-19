cafd832e24ccdc1a56e100a12b03ca2182cab275
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-19 00:18:52 +0000

    Removed unnecessary AccountContext

--- src/contexts/Account.context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-/* eslint-disable */
-export const AccountContext = createContext({
-  token: '',
-  setToken: (s: string) => {},
-  userId: '',
-  setUserId: (s: string) => {},
-});
