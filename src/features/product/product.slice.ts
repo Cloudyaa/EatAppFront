8a21c30d964031281a855b24af666ee227b279cf
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-20 23:56:00 +0000

    Created product slice with fetch products function

---
+++ src/features/product/product.slice.ts
@@ -0,0 +1,58 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { ErrorResponse, SimpleProductEntity } from 'types';
+
+interface Product extends SimpleProductEntity {
+  qtyInBasket: number;
+}
+
+interface ProductState {
+  products: Product[] | null;
+  status: 'idle' | 'loading' | 'success' | 'failed';
+  error: ErrorResponse | null;
+}
+
+const initialState: ProductState = {
+  products: null,
+  status: 'idle',
+  error: null,
+};
+
+export const fetchProducts = createAsyncThunk<Product[], string, { rejectValue: ErrorResponse }>(
+  'products/fetchProducts',
+  async (path, { rejectWithValue }) => {
+    const res = await fetch(path);
+    if (res.ok) {
+      const data: SimpleProductEntity[] = await res.json();
+      return data.map((product) => ({
+        ...product,
+        qtyInBasket: 0,
+      }));
+    } else {
+      const errorData: ErrorResponse = await res.json();
+      return rejectWithValue(errorData);
+    }
+  },
+);
+
+export const productSlice = createSlice({
+  name: 'products',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.products = action.payload;
+        state.status = 'success';
+      })
+
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = 'failed';
+        if (action.payload) {
+          state.error = action.payload;
+        }
+      });
+  },
+});
