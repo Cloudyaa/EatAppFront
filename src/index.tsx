6539715ad2001b754e00e34030583fd3ceb6a681
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-20 23:57:37 +0000

    Packed app with store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { CookiesProvider } from 'react-cookie';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 import './styles/css/style.css';
-import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <CookiesProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </CookiesProvider>
+    </Provider>
   </React.StrictMode>,
 );
 
