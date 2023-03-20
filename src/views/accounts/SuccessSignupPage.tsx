5226e3782130ba427489f135eb4f13a169a6a49b
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-20 19:30:38 +0000

    Fixed spacing between elements

--- src/views/accounts/SuccessSignupPage.tsx
+++ src/views/accounts/SuccessSignupPage.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { ButtonLink, SectionHeader, SectionWrapper, SubPageWrapper } from 'components';
+import { ButtonLink, SectionHeader, SectionWrapper, SpaceFix, SubPageWrapper } from 'components';
+import { useViewport } from 'hooks';
 
 export const SuccessSignupPage = () => {
+  const { viewportWidth, breakpoint } = useViewport();
+
   return (
     <SectionWrapper>
       <SectionHeader>Success!</SectionHeader>
-      <SubPageWrapper>
+      <SubPageWrapper addSx={{ marginBlock: 5 }}>
         <h3>Your account has been created successfully</h3>
+        {viewportWidth > breakpoint ? <SpaceFix /> : null}
         <p>You can now log into your account</p>
         <ButtonLink to={'/account/login'} size="sm" color="light">
           Go to login page
