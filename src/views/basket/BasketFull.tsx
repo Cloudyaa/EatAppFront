bb5d33675abc28d7f262d42985997bf5a6c7fa3e
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-20 23:54:28 +0000

    Created base of full basket view

--- src/views/basket/BasketFull.tsx
+++ src/views/basket/BasketFull.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import { SectionHeader, SectionWrapper } from 'components';
+import { ProductCardMedium, SectionHeader, SectionWrapper } from 'components';
+import { useSkeletons } from '../../hooks/useSkeletons';
+import { SimpleProductEntity } from 'types';
+import { EmptyBasketView } from './EmptyBasketView';
 
 export const BasketFull = () => {
+  const { skeletons } = useSkeletons('md', 3);
+
+  const basket: SimpleProductEntity[] = [];
+
   return (
     <SectionWrapper classes="basket__full">
       <SectionHeader>Your basket</SectionHeader>
+      {basket.length === 0 ? (
+        <EmptyBasketView />
+      ) : (
+        <div className="auto-grid-container">
+          {basket.map((one) => (
+            <ProductCardMedium name={one.name} price={one.price} key={one.productId} />
+          ))}
+        </div>
+      )}
     </SectionWrapper>
   );
 };
