8af40f4b2291c663560054be74b643c83aab0563
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-23 22:55:16 +0000

    Added missing key property

--- src/hooks/useSkeletons.tsx
+++ src/hooks/useSkeletons.tsx
@@ -5,7 +5,7 @@ export const useSkeletons = (size: 'sm' | 'md', qty: number) => {
   const skeletons: JSX.Element[] = [];
   if (size === 'md') {
     for (let i = 0; i < qty; i++) {
-      skeletons.push(<SkeletonMedium />);
+      skeletons.push(<SkeletonMedium key={i} />);
     }
   }
 
