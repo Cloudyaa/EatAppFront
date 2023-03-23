123dadf6a8f610d2556c5c628f7acf53922797de
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-23 22:48:09 +0000

    Moved styles from scss to emotion styled components and finished styling ProductCard

---
+++ src/styles/emotion-styled-components/ProductCard/index.ts
@@ -0,0 +1,2 @@
+export * from './CardWrapper.styled';
+export * from './CardParts';
---
+++ src/styles/emotion-styled-components/index.ts
@@ -0,0 +1,5 @@
+export * from './Badge';
+export * from './Button';
+export * from './ProductCard';
+export * from './Basket';
+export * from './Grid';
