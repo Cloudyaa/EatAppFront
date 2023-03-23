464086b5c5415ca109fcead21e058067b0e6bfcc
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-23 13:30:46 +0000

    Added basket slice to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { productSlice } from '../features/product/product.slice';
+import { useDispatch } from 'react-redux';
+import { basketSlice } from 'features/basket';
+import { productSlice } from 'features/product';
 
 export const store = configureStore({
   reducer: {
     products: productSlice.reducer,
+    basket: basketSlice.reducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
