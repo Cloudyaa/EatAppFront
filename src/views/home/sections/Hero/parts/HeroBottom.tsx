58bee3e410e875773959d15e8e4319b878e8070c
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-26 16:03:19 +0100

    Wrapped button in styled component

--- src/views/home/sections/Hero/parts/HeroBottom.tsx
+++ src/views/home/sections/Hero/parts/HeroBottom.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Image } from 'components';
-import { ButtonLinkStyled } from 'styles';
+import { ButtonLinkStyled, ButtonWrapperStyled } from 'styles';
 
 export const HeroBottom = () => {
   return (
     <div className="home__hero-bottom">
       <Image name={'hero-left'} alt={'orange image'} addClass="home__hero-bottom-image-left" />
-      <div className="home__hero-bottom-btn">
-        <ButtonLinkStyled to={'/products/search'} color={'light'} size={'large'}>
+      <ButtonWrapperStyled>
+        <ButtonLinkStyled to="/products/search" color="light" size="large">
           See our products
         </ButtonLinkStyled>
-      </div>
+      </ButtonWrapperStyled>
     </div>
   );
 };
