a100c5cbd49d5b145e04419518090fc8114fc435
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-27 19:59:18 +0100

    Removed margin

--- src/components/Navbar/NavbarParts/NavPartWrapper.styled.ts
+++ src/components/Navbar/NavbarParts/NavPartWrapper.styled.ts
@@ -2,13 +2,11 @@ import styled from '@emotion/styled';
 
 export const NavPartWrapperStyled = styled.div `
   --nav-el-wrapper-direction: row;
-  --nav-el-wrapper-margin: 0 0 0 2rem;
   --nav-el-wrapper-align: center;
   --nav-el-wrapper-justify: center;
   --nav-el-wrapper-gap: 2rem;
 
   @media (max-width: 800px) {
-    --nav-el-wrapper-margin: 0;
     --nav-el-wrapper-direction: column;
     --nav-el-wrapper-gap: 2rem;
   }
@@ -18,7 +16,6 @@ export const NavPartWrapperStyled = styled.div `
   justify-content: var(--nav-el-wrapper-justify);
   align-items: var(--nav-el-wrapper-align);
   gap: var(--nav-el-wrapper-gap);
-  margin: var(--nav-el-wrapper-margin);
   container-type: inline-size;
   
   & > * {
