d6db056b0116b8940dc31fba745464a3d39c5fde
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-30 23:39:20 +0100

    Created FatFont styled component

---
+++ src/components/styled/Text/FatFont.styled.ts
@@ -0,0 +1,8 @@
+import { Typography } from '@mui/material';
+import { styled } from '@mui/system';
+
+export const FatFontStyled = styled(Typography)`
+  font-weight: 900;
+  font-size: max(1.4rem, 2cqi);
+  padding-block: 1.3rem;
+`;
