04a36fec40711b495ac94345a61a5c0b4083389f
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-30 19:50:37 +0100

    Exported Browse products button to separate component

---
+++ src/components/common/Button/BrowseProductsBtn.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ButtonLinkStyled } from 'components';
+
+export const BrowseProductsBtn = () => (
+  <ButtonLinkStyled size="large" to="/products/search">
+    Browse products
+  </ButtonLinkStyled>
+);
--- src/components/common/Button/index.ts
+++ src/components/common/Button/index.ts
@@ -1,3 +1,4 @@
 export * from './ButtonFull';
+export * from './BrowseProductsBtn';
 export * from './Button.styled';
 export * from './ButtonLink.styled';
