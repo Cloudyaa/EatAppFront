1fde8ef99b8553ac78324167e84f6acfadc066db
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-31 22:33:32 +0100

    Added medium size

--- src/components/common/Button/BrowseProductsBtn.tsx
+++ src/components/common/Button/BrowseProductsBtn.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { ButtonLinkStyled } from 'components';
 
-export const BrowseProductsBtn = () => (
-  <ButtonLinkStyled size="large" to="/products/search">
+interface Props {
+  small?: boolean;
+  medium?: boolean;
+}
+
+export const BrowseProductsBtn = ({ small, medium }: Props) => (
+  <ButtonLinkStyled size={small ? 'small' : medium ? 'medium' : 'large'} to="/products/search">
     Browse products
   </ButtonLinkStyled>
 );
