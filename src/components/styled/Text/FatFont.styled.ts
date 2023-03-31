064f5fb7590975581825e4fa02d5092fff95d80d
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-31 23:02:51 +0100

    Added dynamic color

--- src/components/styled/Text/FatFont.styled.ts
+++ src/components/styled/Text/FatFont.styled.ts
@@ -1,8 +1,14 @@
 import { Typography } from '@mui/material';
 import { styled } from '@mui/system';
+import { colors } from 'styles';
+
+interface Props {
+  light?: boolean;
+}
 
 export const FatFontStyled = styled(Typography)`
   font-weight: 900;
   font-size: max(1.4rem, 2cqi);
   padding-block: 1.3rem;
+  color: ${({ light }: Props) => (light ? colors.secondary.main : 'inherit')};
 `;
