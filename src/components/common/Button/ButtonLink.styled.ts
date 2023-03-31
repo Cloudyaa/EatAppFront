8d7fe6456c1b45a07699073016b73651f1815e05
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-31 22:34:00 +0100

    Added padding

--- src/components/common/Button/ButtonLink.styled.ts
+++ src/components/common/Button/ButtonLink.styled.ts
@@ -1,4 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { ButtonStyled } from './Button.styled';
+import styled from '@emotion/styled';
 
-export const ButtonLinkStyled = ButtonStyled.withComponent(NavLink);
+export const ButtonLinkStyled = styled(ButtonStyled.withComponent(NavLink))`
+  padding-inline: 20px;
+`;
