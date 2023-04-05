9af03a2de9d42d3d3b3b8322f2ddd33836922699
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-04-05 21:31:33 +0100

    Added responsiveness to font-size

--- src/views/home/sections/Contact/styled/ContactInfoTextStyled.ts
+++ src/views/home/sections/Contact/styled/ContactInfoTextStyled.ts
@@ -4,6 +4,7 @@ import { Typography } from '@mui/material';
 export const ContactInfoTextStyled = styled(Typography)`
   max-width: 400px;
   margin: auto;
+  font-size: max(1rem, 1cqi);
 
   @media (max-width: 900px) {
     max-width: 600px;
