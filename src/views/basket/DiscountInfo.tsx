51f4dc553a434e88eefd6a9b7c483e71e3909924
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-04-06 20:30:10 +0100

    Fixed chip display on smaller screens

--- src/views/basket/DiscountInfo.tsx
+++ src/views/basket/DiscountInfo.tsx
@@ -5,7 +5,16 @@ import { colors } from 'styles';
 export const DiscountInfo = () => (
   <Chip
     label={'If you are eligible for discount, it will be applied on the next step'}
-    sx={{ color: colors.dark.light, marginBlock: 2 }}
+    sx={{
+      color: colors.dark.light,
+      marginBlock: 2,
+      height: 'auto',
+      '& .MuiChip-label': {
+        display: 'block',
+        whiteSpace: 'normal',
+        textAlign: 'center',
+        paddingBlock: 1,
+      },
+    }}
   ></Chip>
 );
-
