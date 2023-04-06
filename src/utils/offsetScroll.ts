c8e0908b5f3a92566603b375bdcedb570e4bfa97
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-04-06 20:46:24 +0100

    Change offset amount

--- src/utils/offsetScroll.ts
+++ src/utils/offsetScroll.ts
@@ -1,6 +1,6 @@
 export const offsetScroll = (el: Element) => {
   const yCoordinate = el.getBoundingClientRect().top + window.scrollY;
   let yOffset: number;
-  window.innerWidth < 800 ? (yOffset = -80) : (yOffset = -40);
+  window.innerWidth < 800 ? (yOffset = -40) : (yOffset = 0);
   window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
 };
