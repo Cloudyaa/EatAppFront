11b27d4e7b0bc5e03d2a6718e4df346d5920b81f
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-04-11 14:29:37 +0100

    Fixed overflowing buttons

--- src/components/ProductCard/styles/CardParts/CardButtons/CardBtnAdd.styled.ts
+++ src/components/ProductCard/styles/CardParts/CardButtons/CardBtnAdd.styled.ts
@@ -2,5 +2,8 @@ import styled from '@emotion/styled';
 import { ButtonStyled } from 'components';
 
 export const CardBtnAddStyled = styled(ButtonStyled)`
-  padding: 6px 30px;
+  padding: 2px 20px;
+  @media (max-width: 800px) {
+    padding: 4px 20px;
+  }
 `;
