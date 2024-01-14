b8955849ba3303a7f6bafe65fdbf450cd1491b25
Cloudyaa <klaudiaaolczyk@gmail.com>
2024-01-14 22:18:38 +0100

    create payment view

--- src/routes/OrderRoutes.tsx
+++ src/routes/OrderRoutes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BasketRedirectView, OrderRedirectView, OrderSuccessView } from 'views';
 import { getRoutes } from './getRoutes';
+import { OrderPaymentView } from '../views/order/checkout/OrderPaymentView';
 
 export const BasketRouter = () => {
   const routes = [
@@ -12,6 +13,11 @@ export const BasketRouter = () => {
     {
       path: '/order/checkout',
       element: <OrderRedirectView />,
+    },
+
+    {
+      path: '/order/payment',
+      element: <OrderPaymentView />,
     },
 
     {
