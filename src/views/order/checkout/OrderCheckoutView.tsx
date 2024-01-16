0dc7b949fb11e2befc7b0b15d0e11594028cb82f
Cloudyaa <klaudiaaolczyk@gmail.com>
2024-01-16 23:15:47 +0100

    extract payment components

--- src/views/order/checkout/OrderCheckoutView.tsx
+++ src/views/order/checkout/OrderCheckoutView.tsx
@@ -1,45 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { useCookies } from 'react-cookie';
 import { RootState } from 'store';
-import { apiUrl } from 'config';
 import { useDiscount } from 'hooks';
-import { OrderDTO, OrderedProductEntity, StripeSessionResponse, SuccessOrderResponse } from 'types';
-import {
-  ButtonStyled,
-  FlexEvenlyWrapper,
-  FlexLink,
-  SectionHeader,
-  SectionWrapper,
-  SubPageWrapper,
-} from 'components';
+import { OrderDTO, OrderedProductEntity } from 'types';
+import { FlexLink, SectionHeader, SectionWrapper, SubPageWrapper } from 'components';
 import { CheckoutTable } from './CheckoutTable';
-import { loadStripe } from '@stripe/stripe-js';
-import { FaCcMastercard, FaCcVisa } from 'react-icons/fa6';
 import { Divider } from '@mui/material';
-import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
-import { useNavigate } from 'react-router-dom';
 
-const STRIPE_SECRET = import.meta.env.VITE_STRIPE_SECRET;
-const stripePromise = loadStripe(STRIPE_SECRET);
+import { PayPal } from './PayPal';
+import { Stripe } from './Stripe';
 
-const initialPaypalOptions = {
-  clientId: 'test',
-  'enable-funding': 'paylater',
-  'disable-funding': 'venmo,card',
-  'data-sdk-integration-source': 'integrationbuilder_sc',
-  currency: 'GBP',
-};
+export interface PaymentProps {
+  userOrder: OrderDTO;
+}
 
 export const OrderCheckoutView = () => {
   const basket = useSelector((state: RootState) => state.basket);
   const { totalAtCheckout } = useDiscount();
   const [cookies] = useCookies();
-  const navigate = useNavigate();
-
-  const [orderId, setOrderId] = useState<string | null>(null);
-  const [orderNumber, setOrderNumber] = useState<number | null>(null);
-  const [paymentCompleted, setPaymentCompleted] = useState<boolean>(false);
 
   const orderedProducts: OrderedProductEntity[] = basket.products.map((product) => {
     return {
@@ -57,191 +36,15 @@ export const OrderCheckoutView = () => {
     userId: cookies.userId,
   };
 
-  const fetchData = async <T, D>(
-    url: string,
-    method: 'GET' | 'POST',
-    body?: D,
-  ): Promise<T | void> => {
-    try {
-      const res = await fetch(url, {
-        method,
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${cookies.token}`,
-        },
-        body: JSON.stringify(body),
-      });
-
-      if (res.ok) {
-        return (await res.json()) as T;
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  // const handleCheckout = async () => {
-  //   try {
-  //     const res = await fetch(`${apiUrl}/user/${cookies.userId}/order/new`, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         Authorization: `Bearer ${cookies.token}`,
-  //       },
-  //       body: JSON.stringify(userOrder),
-  //     });
-  //
-  //     if (res.ok) {
-  //       const data: SuccessOrderResponse = await res.json();
-  //       setOrderId(data.orderId);
-  //       setOrderNumber(data.orderNumber);
-  //     }
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // };
-
-  const handleCheckout = async () => {
-    try {
-      const data = await fetchData<SuccessOrderResponse, OrderDTO>(
-        `${apiUrl}/user/${cookies.userId}/order/new`,
-        'POST',
-        userOrder,
-      );
-
-      if (data) {
-        setOrderId(data.orderId);
-        setOrderNumber(data.orderNumber);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const handleStripePayment = async () => {
-    await handleCheckout();
-
-    if (orderId) {
-      try {
-        const data = await fetchData<StripeSessionResponse, any>(
-          `${apiUrl}/user/${cookies.userId}/order/stripe/${orderId}`,
-          'GET',
-        );
-
-        if (data) {
-          const stripe = await stripePromise;
-
-          await stripe?.redirectToCheckout({
-            sessionId: data.session.id,
-          });
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  };
-
-  const handleCreatePaypalOrder = async () => {
-    try {
-      const data = await fetchData<SuccessOrderResponse, OrderDTO>(
-        `${apiUrl}/user/${cookies.userId}/order/new`,
-        'POST',
-        userOrder,
-      );
-      if (data) {
-        const orderData = data.paypalResponse;
-        if (orderData.id) {
-          setOrderNumber(data.orderNumber);
-          return orderData.id;
-        }
-
-        const errorDetail = orderData?.details?.[0];
-        const errorMessage = errorDetail
-          ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
-          : JSON.stringify(orderData);
-
-        throw new Error(errorMessage);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handlePaypalApprove = async (data: any, actions: any) => {
-    try {
-      const orderData = await fetchData<any, any>(
-        `${apiUrl}/user/${cookies.userId}/order/paypal/${data.orderID}`,
-        'POST',
-      );
-
-      if (orderData) {
-        const errorDetail = orderData?.details?.[0];
-
-        if (errorDetail?.issue === 'INSTRUMENT_DECLINED') {
-          return actions.restart();
-        }
-
-        if (errorDetail) {
-          throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
-        }
-
-        const transaction = orderData.purchase_units[0].payments.captures[0];
-
-        if (transaction.status === 'COMPLETED') {
-          setPaymentCompleted(true);
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    if (paymentCompleted && orderNumber) {
-      navigate(`/basket/order/success/${orderNumber}`);
-    }
-  }, [paymentCompleted, orderNumber]);
-
   return (
     <SectionWrapper classes="order__checkout">
       <SectionHeader>Checkout</SectionHeader>
       <SubPageWrapper>
         <h3>Your order summary</h3>
         <CheckoutTable />
-
-        <PayPalScriptProvider options={initialPaypalOptions}>
-          <PayPalButtons
-            style={{
-              shape: 'rect',
-              layout: 'vertical',
-            }}
-            createOrder={handleCreatePaypalOrder}
-            onApprove={(data, actions) => handlePaypalApprove(data, actions)}
-          />
-        </PayPalScriptProvider>
-
+        <PayPal userOrder={userOrder} />
         <Divider>OR</Divider>
-
-        <ButtonStyled onClick={handleStripePayment}>
-          <FlexEvenlyWrapper
-            addSx={{
-              alignItems: 'center',
-              gap: 2,
-            }}
-          >
-            <FaCcMastercard />
-            <FaCcVisa />
-            <img
-              src={
-                'https://www.przelewy24.pl/themes/przelewy24/assets/img/base/przelewy24_logo_2022.svg'
-              }
-              width={40}
-              alt={'przelewy24'}
-              loading="lazy"
-            />
-            <img src={'/blik_icon.svg'} width={40} alt={'przelewy24'} loading="lazy" />
-          </FlexEvenlyWrapper>
-        </ButtonStyled>
+        <Stripe userOrder={userOrder} />
         <FlexLink to={'/basket'} text={'Something wrong?'}>
           Back to basket
         </FlexLink>
---
+++ src/views/order/checkout/Stripe.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { OrderDTO, StripeSessionResponse, SuccessOrderResponse } from 'types';
+import { PaymentProps } from 'views';
+import { loadStripe } from '@stripe/stripe-js';
+import { apiUrl } from 'config';
+import { useFetchData } from 'hooks';
+import { useCookies } from 'react-cookie';
+import { ButtonStyled, FlexEvenlyWrapper } from 'components';
+import { FaCcMastercard, FaCcVisa } from 'react-icons/fa6';
+
+const STRIPE_SECRET = import.meta.env.VITE_STRIPE_SECRET;
+const stripePromise = loadStripe(STRIPE_SECRET);
+
+export const Stripe = ({ userOrder }: PaymentProps) => {
+  const [cookies] = useCookies();
+  const fetchData = useFetchData();
+  const [orderId, setOrderId] = useState<string | null>(null);
+
+  const handleCheckout = async () => {
+    try {
+      const data = await fetchData<SuccessOrderResponse, OrderDTO>(
+        `${apiUrl}/user/${cookies.userId}/order/new`,
+        'POST',
+        userOrder,
+      );
+
+      if (data) {
+        setOrderId(data.orderId);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const handleStripePayment = async () => {
+    await handleCheckout();
+
+    if (orderId) {
+      try {
+        const data = await fetchData<StripeSessionResponse, null>(
+          `${apiUrl}/user/${cookies.userId}/order/stripe/${orderId}`,
+          'GET',
+        );
+
+        if (data) {
+          const stripe = await stripePromise;
+
+          await stripe?.redirectToCheckout({
+            sessionId: data.session.id,
+          });
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
+
+  return (
+    <ButtonStyled onClick={handleStripePayment}>
+      <FlexEvenlyWrapper
+        addSx={{
+          alignItems: 'center',
+          gap: 2,
+        }}
+      >
+        <FaCcMastercard />
+        <FaCcVisa />
+        <img
+          src={
+            'https://www.przelewy24.pl/themes/przelewy24/assets/img/base/przelewy24_logo_2022.svg'
+          }
+          width={40}
+          alt={'przelewy24'}
+          loading="lazy"
+        />
+        <img src={'/blik_icon.svg'} width={40} alt={'przelewy24'} loading="lazy" />
+      </FlexEvenlyWrapper>
+    </ButtonStyled>
+  );
+};
