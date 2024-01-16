709a98a0fa963326cfec92d51c720fc3e64875b7
Cloudyaa <klaudiaaolczyk@gmail.com>
2024-01-16 23:25:28 +0100

    add loader

--- src/views/order/checkout/Stripe.tsx
+++ src/views/order/checkout/Stripe.tsx
@@ -7,6 +7,7 @@ import { useFetchData } from 'hooks';
 import { useCookies } from 'react-cookie';
 import { ButtonStyled, FlexEvenlyWrapper } from 'components';
 import { FaCcMastercard, FaCcVisa } from 'react-icons/fa6';
+import { CircularProgress } from '@mui/material';
 
 const STRIPE_SECRET = import.meta.env.VITE_STRIPE_SECRET;
 const stripePromise = loadStripe(STRIPE_SECRET);
@@ -15,9 +16,11 @@ export const Stripe = ({ userOrder }: PaymentProps) => {
   const [cookies] = useCookies();
   const fetchData = useFetchData();
   const [orderId, setOrderId] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleCheckout = async () => {
     try {
+      setIsLoading(true);
       const data = await fetchData<SuccessOrderResponse, OrderDTO>(
         `${apiUrl}/user/${cookies.userId}/order/new`,
         'POST',
@@ -51,6 +54,8 @@ export const Stripe = ({ userOrder }: PaymentProps) => {
         }
       } catch (e) {
         console.error(e);
+      } finally {
+        setIsLoading(false);
       }
     }
   };
@@ -63,17 +68,23 @@ export const Stripe = ({ userOrder }: PaymentProps) => {
           gap: 2,
         }}
       >
-        <FaCcMastercard />
-        <FaCcVisa />
-        <img
-          src={
-            'https://www.przelewy24.pl/themes/przelewy24/assets/img/base/przelewy24_logo_2022.svg'
-          }
-          width={40}
-          alt={'przelewy24'}
-          loading="lazy"
-        />
-        <img src={'/blik_icon.svg'} width={40} alt={'przelewy24'} loading="lazy" />
+        {isLoading ? (
+          <CircularProgress color="primary" size={20} />
+        ) : (
+          <>
+            <FaCcMastercard />
+            <FaCcVisa />
+            <img
+              src={
+                'https://www.przelewy24.pl/themes/przelewy24/assets/img/base/przelewy24_logo_2022.svg'
+              }
+              width={40}
+              alt={'przelewy24'}
+              loading="lazy"
+            />
+            <img src={'/blik_icon.svg'} width={40} alt={'przelewy24'} loading="lazy" />
+          </>
+        )}
       </FlexEvenlyWrapper>
     </ButtonStyled>
   );
